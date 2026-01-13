64b5eb4a9c5f8dc57f7fddd3358e2d6742dacfe4
Idris jose <idrisjose11@gmail.com>
2026-01-13 17:30:51 +0100

    refactor: remove unused slider component and Firebase configuration

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,46 +1,15 @@
-import React from "react";
-import Slider from "react-slick";
+import React from 'react';
+import { useState } from 'react';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+export default function App() {
+  const [step, setStep] = useState(1);
 
-export default function SimpleSlider() {
-   const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  };
-  return (
-   <div className="slider-container">
-  <Slider {...settings}>
-    {[1, 2, 3, 4, 5, 6,7,8].map(num => (
-      <div key={num} className="px-3"> 
-        <div
-          className="
-            bg-black 
-            p-20
-            h-36 
-            flex 
-            items-center 
-            justify-center 
-            text-3xl 
-            font-semibold 
-            text-green-500 
-            shadow-md 
-            transition 
-            duration-300 
-            hover:-translate-y-2 
-            hover:shadow-xl
-          "
-        >
-          {num}
-        </div>
-      </div>
-    ))}
-  </Slider>
-</div>
-
+  const nextStep = () => {
+    setStep(prev => prev + 1);
+  }
+  return (  
+    <div>
+      <h1>Todo App</h1>
+    </div>
   );
 }
